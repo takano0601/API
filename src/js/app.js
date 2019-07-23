e20d478514a270ee421c4313615529241801cfa6
高野　可南 <kana_takano@012grp.co.jp>
2019-07-23 16:26:07 +0900

    モジュールを別に作る

--- src/js/app.js
+++ src/js/app.js
@@ -8,25 +8,13 @@ import fetchFun from './modules/fetch.js'
 
 var tableJs = document.querySelector('.tableJs');
 var tableInner = "";
-var holiday = document.getElementById('holiday');
 // http://api.e-stat.go.jp/rest/2.1/app/json/getStatsList?appId=+7df9336aa4b63e9a82654cde526e6958d5ed8626&lang=J&openYears=201801-201907&statsCode=00130001&searchKind=1
 
 //祝日一覧API
-// fetch("https://holidays-jp.github.io/api/v1/date.json")
-// .then(response=>{
-//     if(response.ok){
-//         return response.text();
-//     }else{
-//         return Promise.reject(new Error('エラーです'));
-//     }
-// })
-// .then(response =>{
-//     const objRes = JSON.parse(response);
-//     createTable(objRes);
-// });
-let fetchPromiss = fetchFun("https://holidays-jp.github.io/api/v1/date.json");
-console.log(fetchPromiss);
-createTable(fetchPromiss);
+fetchFun("https://holidays-jp.github.io/api/v1/date.json")
+.then(response =>{
+    createTable(response);
+});
 
 function createTable(objData){
     Object.keys(objData).forEach(function(key){
@@ -50,17 +38,9 @@ var mm = ('0' + (today.getMonth() + 1)).slice(-2);   //月
 var dd = ('0' + today.getDate()).slice(-2);          //日
 today = `${today.getFullYear()}-${mm}-${dd}`; //今日の日付
 
-fetch("http://api.nhk.or.jp/v2/pg/genre/400/g1/0409/" + today + ".json?key=" + NHKpass)
-.then(res =>{
-    if(res.ok){
-        return res.text();
-    }else{
-        return Promise.reject(new Error('エラーです'));
-    }
-})
-.then(res=>{
-    const objRes = JSON.parse(res);
-    let programList = objRes.list.g1;
+fetchFun("http://api.nhk.or.jp/v2/pg/genre/400/g1/0409/" + today + ".json?key=" + NHKpass)
+.then(response =>{
+    let programList = response.list.g1;
     progDisplay(programList);
 });
 
@@ -85,7 +65,7 @@ function progDisplay(list){
 }
 
 //音楽番組数をグラフ化
-fetch("http://api.nhk.or.jp/v2/pg/genre/400/e1/0409/2019-07-18.json?key=" + NHKpass)
+fetch("http://api.nhk.or.jp/v2/pg/genre/400/e1/0409/2019-07-23.json?key=" + NHKpass)
 .then(res =>{
     if(res.ok){
         return res.text();
@@ -98,3 +78,5 @@ fetch("http://api.nhk.or.jp/v2/pg/genre/400/e1/0409/2019-07-18.json?key=" + NHKp
     console.log(objRes);    //???
     // let progNum = objRes.
 });
+
+console.log(momnet('2019-12-12'));
--- src/js/modules/fetch.js
+++ src/js/modules/fetch.js
@@ -1,17 +1,19 @@
 
 // URLをfetchして、JSONをパースした状態のpromiseを返す
 export default function fetchFun(url){
-    fetch(url)
-    .then(response=>{
-        if(response.ok){
-            return response.text();
-        }else{
-            return Promise.reject(new Error('エラーです'));
-        }
-    })
-    .then(response =>{
-        const objRes = JSON.parse(response);
-        return objRes;
+    return new Promise((resolve,reject)=>{
+        fetch(url)
+        .then(response =>{
+            if(response.ok){
+                return response.text();
+            }else{
+                return reject(new Error('エラーです'));
+            }
+        })
+        .then(response =>{
+            const resObj = JSON.parse(response);
+            return resolve(resObj);
+        })
     });
-
 };
+
